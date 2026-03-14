b0bcf645b71cd47d522236a1fbfc8ac899dabeaf
Tariq Yunusa <tariikhyunusa@gmail.com>
2026-03-14 13:11:24 +0100

    fix: fixed scale-animation

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -15,15 +15,49 @@ export default function Hero() {
   const doorRef = useRef(null);
   const doorIconRef = useRef(null);
   const contentRef = useRef(null);
+  const mainContentRef = useRef(null)
+  const paragraphRef = useRef(null)
+  const buttonRef = useRef(null)
+  const mainContentHeadRef = useRef(null)
+
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: "top top",
+        end: "+=1500",
+        scrub: true,
+        pin: true
+      }
+    })
+    
+    tl.to(
+      doorRef.current, {
+        width: "100vw",
+        height: "120vh",
+        borderRadius: 0,
+        ease: "none",
+        duration: 1
+      }
+    )
+    .to(
+      [mainContentRef.current, paragraphRef.current, mainContentHeadRef.current, buttonRef.current], {
+        opacity: 0,
+        ease: "none",
+        duration: 0.3
+      }, "<"
+    )
+  },[])
 
 
 
   return (
     <section className={styles.home_section} ref={sectionRef}>
       <main className={styles.home__main}>
-        <div className={styles.home__header_container}>
+        <div className={styles.home__header_container} >
           <h1 className={styles.home__header}>
-            The Internet,
+            <span  ref={mainContentHeadRef}>The Internet,</span>
             <span className={styles.home__header_icons}>
               <div ref={doorRef} className={styles.home__header_span_image}>
                 <span ref={doorIconRef} className={styles.door_icon}>
@@ -34,18 +68,21 @@ export default function Hero() {
                   ref={contentRef}
                   className={styles.next_section_content}
                 >
+                
                 </div>
               </div>
             </span>
-            Without the
+            <span  ref={mainContentRef}>Without the Distractions</span>
           </h1>
         </div>
 
-        <p className={styles.home__paragraph}>
+        <p className={styles.home__paragraph} ref={paragraphRef}>
           Get more done with Lockli.
         </p>
 
-        <Button />
+        <div ref={buttonRef}>
+          <Button />
+        </div>
       </main>
     </section>
   );
