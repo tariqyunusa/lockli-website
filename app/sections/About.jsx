6b7299e225faa57be5a41645961e48fe179a446b
Tariq Yunusa <tariikhyunusa@gmail.com>
2026-03-15 14:22:31 +0100

    animation: added text scroll opacity

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -1,10 +1,35 @@
-import styles from "../styles/About.module.css"
-export default function About() {
-    return(
-        <section className={styles.about__section}>
-        <p>
-            Lockli is an attention management system that helps users structure focused work,understand their online behavior through analytics, and enforce boundaries by limiting access to distracting websites.
-        </p>
-        </section>
-    )
+"use client";
+import styles from "../styles/About.module.css";
+
+export default function About({ refs }) {
+
+  const splitWords = (phrase) => {
+    return phrase.split(" ").map((word, i) => (
+      <span key={word + "_" + i} className={styles.word}>
+        {splitLetters(word)}
+      </span>
+    ));
+  };
+
+  const splitLetters = (word) => {
+    return word.split("").map((letter, i) => (
+      <span
+        key={letter + "_" + i}
+        ref={(el) => el && refs.current.push(el)}
+        className={styles.char}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
+  return (
+    <section className={styles.about__section}>
+      <div className={styles.about__section_content}>
+        {splitWords(
+          "Lockli is an attention management system that helps users structure focused work, understand their online behavior through analytics, and enforce boundaries by limiting access to distracting websites."
+        )}
+      </div>
+    </section>
+  );
 }
