e7425dd9ac9c45d2ab7dd18fe9356f65e67572d9
Tariq Yunusa <tariikhyunusa@gmail.com>
2026-03-16 11:49:46 +0100

    animation: added icon pop animation

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -1,14 +1,36 @@
 "use client";
 import styles from "../styles/About.module.css";
 
-export default function About({ refs }) {
+export default function About({ refs, iconRefs }) {
+  const icons = {
+    work: 0,
+    analytics: 1,
+    websites: 2,
+  };
+  const iconColors = ["#6da7c7", "#219EBC", "#81B29A"];
 
   const splitWords = (phrase) => {
-    return phrase.split(" ").map((word, i) => (
-      <span key={word + "_" + i} className={styles.word}>
-        {splitLetters(word)}
-      </span>
-    ));
+    return phrase.split(" ").map((word, i) => {
+      const cleanWord = word.replace(",", "").replace(".", "");
+
+      return (
+        <span key={word + "_" + i} className={styles.word}>
+          {splitLetters(word)}
+
+          {icons[cleanWord] !== undefined && (
+            <span
+              ref={(el) => (iconRefs.current[icons[cleanWord]] = el)}
+              className={styles.inline_icon}
+              style={{ backgroundColor: iconColors[icons[cleanWord]] }}
+            >
+              {icons[cleanWord] === 0 && "🔗"}
+              {icons[cleanWord] === 1 && "📈"}
+              {icons[cleanWord] === 2 && "🔒"}
+            </span>
+          )}
+        </span>
+      );
+    });
   };
 
   const splitLetters = (word) => {
@@ -27,7 +49,7 @@ export default function About({ refs }) {
     <section className={styles.about__section}>
       <div className={styles.about__section_content}>
         {splitWords(
-          "Lockli is an attention management system that helps users structure focused work, understand their online behavior through analytics, and enforce boundaries by limiting access to distracting websites."
+          "Lockli is an attention management system that helps users structure focused work, understand their online behavior through analytics, and enforce boundaries by limiting access to distracting websites.",
         )}
       </div>
     </section>
--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -15,109 +15,141 @@ export default function Hero() {
   const doorRef = useRef(null);
   const doorIconRef = useRef(null);
   const contentRef = useRef(null);
-  const mainContentRef = useRef(null)
-  const paragraphRef = useRef(null)
-  const buttonRef = useRef(null)
-  const mainContentHeadRef = useRef(null)
+  const mainContentRef = useRef(null);
+  const paragraphRef = useRef(null);
+  const buttonRef = useRef(null);
+  const mainContentHeadRef = useRef(null);
   const letterRefs = useRef([]);
-
-
- useEffect(() => {
+  const iconRefs = useRef([]);
+  iconRefs.current = [];
   const letters = letterRefs.current;
+  const iconLetterIndex = [62, 112, 168];
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: sectionRef.current,
-      start: "top top",
-      end: "+=3000",
-      scrub: true,
-      pin: true
-    }
-  });
+  useEffect(() => {
+    const letters = letterRefs.current;
 
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: "top top",
+        end: "+=3000",
+        scrub: true,
+        pin: true,
+      },
+    });
 
-  tl.to(doorIconRef.current, {
-    opacity: 0,
-    ease: "none",
-    duration: 0.3
-  });
-
-
-  tl.to(
-    [mainContentRef.current, paragraphRef.current, mainContentHeadRef.current, buttonRef.current],
-    {
+    gsap.set(iconRefs.current, {
+      opacity: 0,
+      width: 0,
+      scale: 0.2
+    });
+    tl.to(doorIconRef.current, {
       opacity: 0,
       ease: "none",
-      duration: 0.3
-    }
-  );
+      duration: 0.3,
+    });
 
+    tl.to(
+      [
+        mainContentRef.current,
+        paragraphRef.current,
+        mainContentHeadRef.current,
+        buttonRef.current,
+      ],
+      {
+        opacity: 0,
+        ease: "none",
+        duration: 0.3,
+      },
+    );
 
-  tl.to(doorRef.current, {
-    width: "110vw",
-    height: "110vh",
-    borderRadius: 0,
-    ease: "none",
-    duration: 1
-  });
-
-  tl.to(contentRef.current, {
-    opacity: 1,
-    ease: "none",
-    duration: 0.2
-  });
-
-  tl.to(
-    {},
-    {
-      duration: 2,
+    tl.to(doorRef.current, {
+      width: "110vw",
+      height: "110vh",
+      borderRadius: 0,
       ease: "none",
-      onUpdate: function () {
-        const progress = this.progress();
+      duration: 1,
+    });
 
-        letters.forEach((el, i) => {
-          const threshold = i / letters.length;
+    tl.to(contentRef.current, {
+      opacity: 1,
+      ease: "none",
+      duration: 0.2,
+    });
 
-          const opacity = gsap.utils.clamp(
-            0.2,
-            1,
-            (progress - threshold) * letters.length
-          );
+    tl.to(
+      {},
+      {
+        duration: 2,
+        ease: "none",
+        onUpdate: function () {
+          const progress = this.progress();
+          const totalLetters = letters.length;
 
-          gsap.set(el, { opacity });
-        });
-      }
-    }
-  );
-}, []);
- 
+          
+          letters.forEach((el, i) => {
+            const threshold = i / totalLetters;
 
+            const opacity = gsap.utils.clamp(
+              0.2,
+              1,
+              (progress - threshold) * totalLetters,
+            );
 
+            gsap.set(el, { opacity });
+          });
+
+          
+          iconRefs.current.forEach((icon, i) => {
+            const trigger = (iconLetterIndex[i] + 5) / totalLetters;
+
+            if (progress > trigger) {
+              gsap.to(icon, {
+                width: 75,
+                opacity: 1,
+                duration: 0.35,
+                ease: "cubic-bezier(.34,1.56,.64,1)",
+                scale: 1,
+                overwrite: true,
+              });
+            } else {
+              gsap.to(icon, {
+                width: 0,
+                opacity: 0,
+                duration: 0.25,
+                ease: "cubic-bezier(.4,0,.2,1)",
+                overwrite: true,
+              });
+            }
+          });
+        },
+      },
+    );
+  }, []);
 
   return (
     <section className={styles.home_section} ref={sectionRef}>
       <main className={styles.home__main}>
-        <div className={styles.home__header_container} >
+        <div className={styles.home__header_container}>
           <h1 className={styles.home__header}>
-            <span  ref={mainContentHeadRef}>The Internet,</span>
+            <span ref={mainContentHeadRef}>The Internet,</span>
             <span className={styles.home__header_icons}>
               <div ref={doorRef} className={styles.home__header_span_image}>
                 <span ref={doorIconRef} className={styles.door_icon}>
                   {doorIcon}
                 </span>
 
-                <div
-                  
-                  className={styles.next_section_content}
-                >
-                  <div className={styles.next_section_content_inner} ref={contentRef}>
-                <About refs={letterRefs}/>
-
+                <div className={styles.next_section_content}>
+                  <div
+                    className={styles.next_section_content_inner}
+                    ref={contentRef}
+                  >
+                    <About refs={letterRefs} iconRefs={iconRefs} />
                   </div>
                 </div>
               </div>
             </span>
-            <span  ref={mainContentRef}>Without the Distractions</span>
+            <span ref={mainContentRef}>Without the Distractions</span>
           </h1>
         </div>
 
